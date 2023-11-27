025577c1c9e8a422df72147d6563e1dd2ce12fa5
GURUVEDHANTH S <officialvedhanth@gmail.com>
2023-11-27 12:19:45 +0530

    Deployed

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -6,7 +6,6 @@ class AppClass extends Component {
     return (
         <div>
             <img key={this.props.id} src={this.props.imgUrl} alt="" />
-            <div>Hello</div>
         </div>
     )
   }
